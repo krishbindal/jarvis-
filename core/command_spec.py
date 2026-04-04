15bd77b6a6d1cf9b1818f272647e96800eedde53
Krish bindal <krish.23scse1012070@galgotiasuniversity.ac.in>
2026-04-04 18:58:53 +0530

    Add command specifications for file operations

---
+++ core/command_spec.py
@@ -0,0 +1,55 @@
+COMMAND_SPEC = [
+    {
+        "action": "list_files",
+        "phrases": [
+            "list files in",
+            "show files in",
+            "list files"
+        ],
+        "default_path": "~/Downloads"
+    },
+    {
+        "action": "create_folder",
+        "phrases": [
+            "create folder",
+        ]
+    },
+    {
+        "action": "delete_file",
+        "phrases": [
+            "delete file",
+            "remove file"
+        ]
+    },
+    {
+        "action": "move_file",
+        "phrases": [
+            "move file"
+        ]
+    },
+    {
+        "action": "copy_file",
+        "phrases": [
+            "copy file"
+        ]
+    },
+    {
+        "action": "rename_file",
+        "phrases": [
+            "rename file"
+        ]
+    },
+    {
+        "action": "search_file",
+        "phrases": [
+            "search file",
+            "find file"
+        ]
+    },
+    {
+        "action": "file_info",
+        "phrases": [
+            "file info"
+        ]
+    }
+]
