5422de4a8bb6c4c555d20773d32a803bf806f1e5
Krish bindal <krish.23scse1012070@galgotiasuniversity.ac.in>
2026-04-04 19:08:50 +0530

    Create network_spec.py

---
+++ core/network_spec.py
@@ -0,0 +1,30 @@
+NETWORK_SPEC = [
+    {
+        "action": "download_file",
+        "phrases": [
+            "download",
+            "download file",
+            "download file from"
+        ]
+    },
+    {
+        "action": "download_video",
+        "phrases": [
+            "download video",
+            "download youtube video"
+        ]
+    },
+    {
+        "action": "convert_to_mp3",
+        "phrases": [
+            "convert to mp3",
+            "convert audio to mp3"
+        ]
+    },
+    {
+        "action": "convert_to_pdf",
+        "phrases": [
+            "convert to pdf"
+        ]
+    }
+]
